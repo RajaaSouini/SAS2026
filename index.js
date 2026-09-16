0b82c89b30e1279a9de593e9e40cdbb8b2149033
RajaaSouini <rajaasouini@gmail.com>
2026-09-16 17:20:13 +0100

    tp1

---
+++ index.js
@@ -0,0 +1,15 @@
+//et 1
+console.log("SkyLine Airlines - Check-In");
+//et 2
+console.log("Passenger: karim Benali" , "|" , "Flight:" , "SL204");
+//et 3
+console.log("Seat :" + 14);
+console.log("Seat :" + 14 + "A");
+//et 4
+console.log(45 < 60);
+//et 5
+console.log("Priority boarding:" , 1200 >= 1000);
+//et 6
+console.log(typeof(349.99));
+//et 7
+console.log("Gate: " + 12 + "Status: Boarding");
