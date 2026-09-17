39babb317df01537845b92c63a00845c83e30cc0
RajaaSouini <rajaasouini@gmail.com>
2026-09-17 12:23:35 +0100

    day2

---
+++ day2.js
@@ -0,0 +1,33 @@
+//déclaration d une variable qui peut etre modifier
+let a = 5; 
+//déclaration d'une constante
+const b = 8 ;
+a += 9 ;
+//condition
+if (a > 8){
+    console.log("a est supérieure à 8");
+}else{
+    console.log("a est inférieure à 8");
+}
+
+//Utilisation de la méthode switch
+
+let day = "Monay";
+switch (day){
+    case "Monday":
+        console.log("it's the first day of the week");
+        break;
+    case "Tuesday" :
+        console.log("It's the second day of the day");
+        break;
+    case "wednesday":
+        console.log("it's the third day of the week");
+        break;
+    default :
+    console.log("weekend");
+}
+
+//Les opérateurs arithmetique et logique 
+if (a += 3 >= 6 || a === 8){
+    console.log("vrai");
+}
