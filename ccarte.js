c389459cc45dd19c9f55546fe496f1704cd69599
RajaaSouini <rajaasouini@gmail.com>
2026-09-18 15:10:14 +0100

    carte

---
+++ ccarte.js
@@ -0,0 +1,17 @@
+for (let i = 0; i <= 3; i++) {
+    let row = "";
+    for (let j = 0; j <= 3; j++) {
+        let a = "";
+        if ((i === 0 || i === 3) && (j === 0 || j === 3)) {
+            a = "corner";
+        } else if ((i === 1 || i === 2) && (j === 1 || j === 2)) {
+            a = "inside";
+        } else {
+            a = "edge";
+        }
+        
+        row += a + " ";
+    }
+    console.log(row);
+}
+
